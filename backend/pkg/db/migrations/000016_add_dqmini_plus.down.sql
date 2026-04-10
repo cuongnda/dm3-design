0ad15d8fe9753ed8b5a8f923d0fe4fce56115bd7
thehiep-lai <hiep@duali.com>
2026-04-10 11:59:50 +0700

    update device model dqmini+

---
+++ backend/pkg/db/migrations/000016_add_dqmini_plus.down.sql
@@ -0,0 +1,19 @@
+-- Remove dqmini_plus device model
+
+ALTER TABLE dm3_devices.devices DROP CONSTRAINT IF EXISTS chk_device_model;
+ALTER TABLE dm3_devices.devices
+    ADD CONSTRAINT chk_device_model CHECK (model IS NULL OR model IN (
+        'ra08','ba8300','df970','dq200','dq8500','icu970',
+        'icu300n','ipopx','itouch_pop_x','icu400',
+        'camera_dc','cctv',
+        'door_sensor','de960','de950'
+    ));
+
+ALTER TABLE dm3_devices.firmware DROP CONSTRAINT IF EXISTS chk_firmware_device_type;
+ALTER TABLE dm3_devices.firmware
+    ADD CONSTRAINT chk_firmware_device_type CHECK (device_type IN (
+        'ra08','ba8300','df970','dq200','dq8500','icu970',
+        'icu300n','ipopx','itouch_pop_x','icu400',
+        'camera_dc','cctv',
+        'door_sensor','de960','de950'
+    ));
